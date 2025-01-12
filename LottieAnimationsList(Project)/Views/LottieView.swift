3c7dc0c2759d9c297426971c4111069813a7d86b
Thomas Chao <Tchao@lcs.on.ca>
2025-01-12 11:23:31 -0500

    Added Mr. Gordon's helper view.

---
+++ LottieAnimationsList(Project)/Views/LottieView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LottieAnimationsList(Project)
+//
+//  Created by Yuk Yeung Chao on 2025-01-12.
+//
+
+import SwiftUI
+
+struct AnimationsListView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    AnimationsListView()
+}
