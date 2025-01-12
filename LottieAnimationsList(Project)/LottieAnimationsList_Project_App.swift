77a60ff66e8c009f410179ef6f84171b0b493d80
Thomas Chao <Tchao@lcs.on.ca>
2025-01-12 11:12:30 -0500

    Organized project and created a list view to show interesting animations.

--- LottieAnimationsList(Project)/LottieAnimationsList_Project_App.swift
+++ LottieAnimationsList(Project)/LottieAnimationsList_Project_App.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct LottieAnimationsList_Project_App: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            AnimationsListView()
         }
     }
 }
