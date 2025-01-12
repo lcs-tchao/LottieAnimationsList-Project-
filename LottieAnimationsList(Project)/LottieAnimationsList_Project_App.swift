ffd4771a383225e46ff277fe0bb674adb6e01eed
Thomas Chao <Tchao@lcs.on.ca>
2025-01-12 11:09:08 -0500

    Initial Commit

---
+++ LottieAnimationsList(Project)/LottieAnimationsList_Project_App.swift
@@ -0,0 +1,17 @@
+//
+//  LottieAnimationsList_Project_App.swift
+//  LottieAnimationsList(Project)
+//
+//  Created by Yuk Yeung Chao on 2025-01-12.
+//
+
+import SwiftUI
+
+@main
+struct LottieAnimationsList_Project_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
