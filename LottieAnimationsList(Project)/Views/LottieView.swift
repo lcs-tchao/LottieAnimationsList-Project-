1e32ccb9f5359abfd0fe265248a7542f73fdb1c4
Thomas Chao <Tchao@lcs.on.ca>
2025-01-12 11:23:39 -0500

    Added Mr. Gordon's helper view.

--- LottieAnimationsList(Project)/Views/LottieView.swift
+++ LottieAnimationsList(Project)/Views/LottieView.swift
@@ -1,24 +1,53 @@
+
 //
-//  ContentView.swift
-//  LottieAnimationsList(Project)
+//  LottieView.swift
+//  LottieAnimationsTest
 //
-//  Created by Yuk Yeung Chao on 2025-01-12.
+//  Created by Russell Gordon on 2023-01-27.
 //
 
+import UIKit
 import SwiftUI
+import Lottie
 
-struct AnimationsListView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+struct LottieView: UIViewRepresentable {
+
+    @State var animationNamed: String
+
+    func makeUIView(context: UIViewRepresentableContext<LottieView>) -> UIView {
+        
+        let view = UIView(frame: .zero)
+        
+        // Lottie View
+        let animationView = LottieAnimationView()
+        let animation = LottieAnimation.named(animationNamed)
+        animationView.animation = animation
+        animationView.contentMode = .scaleAspectFit
+        animationView.loopMode = .loop
+        animationView.play()
+        
+        // Constraints
+        animationView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(animationView)
+        
+        NSLayoutConstraint.activate([
+            animationView.widthAnchor.constraint(equalTo: view.widthAnchor),
+            animationView.heightAnchor.constraint(equalTo: view.heightAnchor)
+        ])
+        
+        return view
+        
     }
-}
-
-#Preview {
-    AnimationsListView()
+    
+    func updateUIView(_ uiView: UIView, context: Context) {
+        
+    }
+    
+    typealias UIViewType = UIView
+    
+    struct LottieView_Previews: PreviewProvider {
+        static var previews: some View {
+            LottieView(animationNamed: "782-check-mark-success")
+        }
+    }
 }
